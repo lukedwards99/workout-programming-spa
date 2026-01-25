0f17ba9326e881e7372dba52f2e7d22c719d2aff
lukedwards99 <luke.edwards20@gmail.com>
2026-01-25 11:19:18 -0600

    Setup github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/workout-programming-spa/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
