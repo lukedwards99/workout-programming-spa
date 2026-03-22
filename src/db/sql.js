e6d5c02fd70a1725a02fff49ca7866601fa72f94
lukedwards99 <luke.edwards20@gmail.com>
2026-03-22 14:03:35 -0500

    feat: basic setup and ideation

--- src/db/sql.js
+++ src/db/sql.js
@@ -1,18 +1,19 @@
 export const createDatabaseSQL= `
 
-  CREATE TABLE IF NOT EXISTS program (
+  CREATE TABLE IF NOT EXISTS mesocycle (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    start_date TEXT NOT NULL,
     program_name TEXT NOT NULL,
     notes TEXT
   );
 
-  CREATE TABLE IF NOT EXISTS days (
+  CREATE TABLE IF NOT EXISTS workouts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    program_id INTEGER NOT NULL,
-    day_name TEXT NOT NULL,
-    day_order INTEGER NOT NULL UNIQUE,
-    notes TEXT
-    FOREIGN KEY (program_id) REFERENCES program(id) ON DELETE CASCADE
+    mesocycle_id INTEGER NOT NULL,
+    workout_name TEXT NOT NULL,
+    workout_order INTEGER NOT NULL UNIQUE,
+    notes TEXT,
+    FOREIGN KEY (mesocycle_id) REFERENCES mesocycle(id) ON DELETE CASCADE
   );
 
   CREATE TABLE IF NOT EXISTS workout_groups (
@@ -24,18 +25,19 @@ export const createDatabaseSQL= `
   CREATE TABLE IF NOT EXISTS exercises (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     workout_group_id INTEGER NOT NULL,
+    tutorial_url TEXT,
     name TEXT NOT NULL,
     notes TEXT,
     FOREIGN KEY (workout_group_id) REFERENCES workout_groups(id) ON DELETE CASCADE
   );
 
-  CREATE TABLE IF NOT EXISTS day_workout_groups (
+  CREATE TABLE IF NOT EXISTS workout_groups_selection (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    day_id INTEGER NOT NULL,
+    workout_id INTEGER NOT NULL,
     workout_group_id INTEGER NOT NULL,
-    FOREIGN KEY (day_id) REFERENCES days(id) ON DELETE CASCADE,
+    FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE,
     FOREIGN KEY (workout_group_id) REFERENCES workout_groups(id) ON DELETE CASCADE,
-    UNIQUE(day_id, workout_group_id)
+    UNIQUE(workout_id, workout_group_id)
   );
 
   CREATE TABLE IF NOT EXISTS workout_sets (
@@ -43,7 +45,7 @@ export const createDatabaseSQL= `
     day_id INTEGER NOT NULL,
     exercise_id INTEGER NOT NULL,
     exercise_order INTEGER NOT NULL,
-    set_order INTEGER NOT NULL,
+    set_number INTEGER NOT NULL,
     reps INTEGER,
     weight REAL,
     rir INTEGER,
@@ -51,6 +53,19 @@ export const createDatabaseSQL= `
     FOREIGN KEY (day_id) REFERENCES days(id) ON DELETE CASCADE,
     FOREIGN KEY (exercise_id) REFERENCES exercises(id) ON DELETE CASCADE
   );
+
+  CREATE TABLE IF NOT EXISTS child_sets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    parent_set_id INTEGER NOT NULL,
+    exercise_id INTEGER NOT NULL,
+    exercise_order INTEGER NOT NULL,
+    reps INTEGER,
+    weight REAL,
+    rir INTEGER,
+    notes TEXT,
+    FOREIGN KEY (parent_set_id) REFERENCES workout_sets(id) ON DELETE CASCADE,
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id) ON DELETE CASCADE
+  ))
 `;
 
 export const deleteDatabaseSQL= `
